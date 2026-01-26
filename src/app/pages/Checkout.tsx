b34fb01a86e8c06edca1f41b26d1f63919428393
Abhik Bangera <abhikbangera@gmail.com>
2026-01-26 21:14:43 +0530

    madechanges

--- src/app/pages/Checkout.tsx
+++ src/app/pages/Checkout.tsx
@@ -51,7 +51,7 @@ export function Checkout() {
     
     // Razorpay integration
     const options = {
-      key: 'YOUR_RAZORPAY_KEY_ID', // Replace with your Razorpay Key ID
+      key: 'rzp_live_S7lrNBZwLJnG0c ', // Replace with your Razorpay Key ID
       amount: Math.round(grandTotal * 100), // Amount in paise
       currency: 'INR',
       name: 'RetroKick',
